231dc08cbdbdbb4f44b782f9f921682fbb547129
Jatin Singh <Jatinsingh0701@gmail.com>
2023-02-27 23:26:26 +0530

    add public file and host in loacl server

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const path = require("path");
+const { join } = require("path");
+// const request = require("request");
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello 3000");
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/signup.html"));
+    
+});
 
 
+app.post("/", (req, res) => {
 
-app.listen(3000,()=>{
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.email;
+
+    console.log(firstName,lastName,email);
+    
+});
+
+app.listen(3000, () => {
     console.log("3000 is good to go");
-})
+});
+
+
+
+// API KEY 
+// 5fa56c79d8d489460835c7da1dbed27e-us12
+// Audience ID
+// 8a35e2a557
