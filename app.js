a03379609426910c926bc308cc719b5f5823b5c9
Jatin Singh <Jatinsingh0701@gmail.com>
2023-03-01 00:48:26 +0530

    add comments

--- app.js
+++ app.js
@@ -2,38 +2,77 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const path = require("path");
-const { join } = require("path");
-// const request = require("request");
 
 const app = express();
 
+// Serve static files from the public folder
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse request bodies encoded in urlencoded format
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Handle GET requests to the root URL
+
 app.get("/", (req, res) => {
+    // Send the signup.html file
+
     res.sendFile(path.join(__dirname + "/signup.html"));
-    
+
 });
 
+// Handle POST requests to the root URL
 
 app.post("/", (req, res) => {
 
-    let firstName = req.body.fname;
-    let lastName = req.body.lname;
-    let email = req.body.email;
+    // Extract the first name, last name, and email from the request body
 
-    console.log(firstName,lastName,email);
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    // Construct the data object to send to the Mailchimp API
+
+    const data = {
+
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    }
+
+    // Convert the data object to JSON
+
+    const jsonData = JSON.stringify(data);
+
+    // TODO: Send the JSON data to the Mailchimp API using an HTTPS request
+
+});
+
+// Start the server and listen on port 3000
+
+app.listen(3000, () => {
+
+    console.log("Server is running on port 3000");
     
 });
 
-app.listen(3000, () => {
-    console.log("3000 is good to go");
-});
+// Mailchimp API Key
+
+// TODO: Replace this with your Mailchimp API Key
+// const apiKey = "5fa56c79d8d489460835c7da1dbed27e-us12";
 
 
+// Mailchimp Audience ID
 
-// API KEY 
-// 5fa56c79d8d489460835c7da1dbed27e-us12
-// Audience ID
-// 8a35e2a557
+// TODO: Replace this with your Mailchimp Audience ID
+
+// const audienceId = "8a35e2a557";
+
