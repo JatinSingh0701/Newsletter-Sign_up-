5add99f3674c5909cf1d78c5e93cc299d9f9d7f3
Jatin Singh <Jatinsingh0701@gmail.com>
2023-03-01 02:20:12 +0530

    complete a website

--- app.js
+++ app.js
@@ -52,27 +52,69 @@ app.post("/", (req, res) => {
 
     const jsonData = JSON.stringify(data);
 
-    // TODO: Send the JSON data to the Mailchimp API using an HTTPS request
+    // Send the JSON data to the Mailchimp API using an HTTPS request
+
+    const apiKey = "5fa56c79d8d489460835c7da1dbed27e-us12";
+    const audienceId = "8a35e2a557";
+
+    const url = `https://us12.api.mailchimp.com/3.0/lists/${audienceId}`;
+
+    const options = {
+        method: "POST",
+        auth: `jatin1:${apiKey}`
+    };
+
+    const request = https.request(url, options, (response) => {
+
+        // Handle response from the Mailchimp API
+
+        if (response.statusCode === 200) {
+            // If successful, send the success.html file
+
+            res.sendFile(path.join(__dirname + "/success.html"));
+        } else {
+            // If not successful, send the failure.html file
+
+            res.sendFile(path.join(__dirname + "/failure.html"));
+        }
+        // Log the response data to the console
+
+        response.on("data", (data) => {
+
+            console.log(JSON.parse(data));
+
+        });
+
+    });
+    // Write the JSON data to the request body and end the request
+
+    request.write(jsonData);
+    request.end();
 
 });
+
+// Handle POST requests to the failure and success URLs by redirecting to the root URL
+
+app.post("/failure", (req, res) => {
+
+res.redirect("/")
+
+})
+
+app.post("/success", (req, res) => {
+
+res.redirect("/")
+
+})
 
 // Start the server and listen on port 3000
 
 app.listen(3000, () => {
 
-    console.log("Server is running on port 3000");
-    
+console.log("Server is running on port 3000");
+
 });
 
-// Mailchimp API Key
-
-// TODO: Replace this with your Mailchimp API Key
-// const apiKey = "5fa56c79d8d489460835c7da1dbed27e-us12";
 
 
-// Mailchimp Audience ID
-
-// TODO: Replace this with your Mailchimp Audience ID
-
-// const audienceId = "8a35e2a557";
 
